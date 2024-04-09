32101318e1b4081014f5f940743b94799c7b82e4
Alim <alim.imkakpamale@gmail.com>
2024-04-09 12:16:44 +0200

    view und inserts

--- SQL/Views.sql
+++ SQL/Views.sql
@@ -1,5 +1,5 @@
 create or replace view view_Fahrzeuge
-as select h.bezeichnung as hersteller, m.bezeichnung as modell, m.getriebe, m.ps, f.fahrbar as status
+as select h.bezeichnung as hersteller, m.bezeichnung as modell, m.getriebe, m.ps, f.status as status
 from tbl_fahrzeuge f, tbl_hersteller h, tbl_modelle m
 where f.modell = m.modell_id 
 AND m.hersteller = h.hersteller_id;
